cd45985c337c61a449ce2007b54f099cdd0bc5ed
Ajay Parameshwar Morade <103033106+AjayMorde@users.noreply.github.com>
2023-10-25 17:52:31 +0530

    Delete connection to my database directory

--- connection to my database/database.js
+++
@@ -1,6 +0,0 @@
-const Sequelize=require('sequelize');
-const sequelize=new Sequelize('ExpenseTrackeApp','root','Ajay@1998',{  
-    dialect:'mysql',
-    host:'localhost'
-})
-module.exports=sequelize;
